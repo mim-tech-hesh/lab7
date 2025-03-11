30e6114e835c437b5f00ebccccaec759c5e8ef66
mim-tech-hesh <mim15-5924@diu.edu.bd>
2025-03-11 11:14:36 +0600

    lab7task2

--- lib/assets/home/pages/home..dart
+++ lib/assets/home/pages/home..dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:icons_plus/icons_plus.dart';
-import 'package:lab7/utils/costant/colors.dart';
+import 'package:lab07/utils/constants/colors.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -8,23 +8,50 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Mrga Shop",
-          style: TextStyle(
-            fontFamily: "DMSans",
-            fontWeight: FontWeight.w700,
-            color:FColors.cceanBlue,
-          ),
-        ),
-        actions:  [
-          Icon(Iconsax.notification_bring_outline),
-          Icon(HeroIcons.shopping_cart),
-        ],
-        centerTitle:true,
-      ),
+      appBar: FAppBar(),
     );
   }
 }
 
-       
+class FAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const FAppBar({
+    super.key,
+  });
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      leading: Icon(Iconsax.arrow_left_2_outline),
+      title: Text(
+        "Mega Shop",
+        style: TextStyle(
+          fontFamily: "DMSans",
+          fontWeight: FontWeight.w700,
+          color: FColors.oceanBlue,
+        ),
+      ),
+      actions: [
+        Container(
+          padding: EdgeInsets.fromLTRB(10, 10, 5, 10),
+          child: Icon(
+            Iconsax.notification_bing_outline,
+            color: FColors.dark,
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.fromLTRB(5, 10, 10, 10),
+          child: Icon(
+            IonIcons.cart,
+            color: FColors.dark,
+          ),
+        ),
+      ],
+      centerTitle: true,
+      elevation: 2,
+      backgroundColor: Colors.white,
+    );
+  }
+}
