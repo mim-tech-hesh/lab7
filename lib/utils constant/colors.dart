05102c6bea1104ac6ab294b7fc6dd455c75f6021
mim-tech-hesh <mim15-5924@diu.edu.bd>
2025-03-11 10:26:14 +0600

    lab7

---
+++ lib/utils constant/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class FColors {
+  //Primary
+  static const primaryBlueOcan = Color(0xFF5CC936);
+  static const primaryOrangeFresh = Color(0xFFFFC120);
+  static const primaryNavyBlack = Color(0xFF0C1A30);
+}
